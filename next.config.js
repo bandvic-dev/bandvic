a97344fc4dd13799731036264f6ff6cd00d6432b
working-web <working.web31@gmail.com>
2022-08-20 14:56:10 +0300

    get rif of firebase deploy configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "imgix",
-    path: "https://bandvic.web.app/",
-  },
+  swcMinify: true
 }
 
 module.exports = nextConfig
